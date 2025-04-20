232429b849edce23efb911db99f193ec92a9f4a0
Crede322 <burhanov774@gmail.com>
2025-04-20 08:07:35 +0500

    feat(reviews): добавил блок с отзывами

---
+++ components/mainpage/reviews/ReviewsSlide.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="px-[16%]">
+    <div class="bg-[var(--color-lightblue)] px-[24px] py-[30px] relative">
+      <h3 class="text-center">{{ textMain }}</h3>
+      <h3 class="text-center mt-[16px] font-bold">{{ reviewerName }}</h3>
+      <!-- <div
+        class="absolute w-[35px] h-[35px] bg-[var(--color-lightblue)] bottom-[-18px] left-1/2 transform -translate-x-1/2 rotate-45"
+      ></div> -->
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  textMain: String,
+  reviewerName: String,
+});
+</script>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,13 @@
     <WhyUs />
     <MainpagePopularServices />
     <MainpageResults />
+    <Reviews />
   </div>
 </template>
 <script setup lang="ts">
 import Hero from '~/components/mainpage/hero/Hero.vue';
 import WhyUs from '~/components/mainpage/whyUs/WhyUs.vue';
+import Reviews from '~/components/mainpage/reviews/Reviews.vue';
 </script>
 <style>
 .container {
