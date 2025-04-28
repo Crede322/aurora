0275674cd120e51b33e103c9354d7270e37f14f5
Crede322 <burhanov774@gmail.com>
2025-04-28 13:14:51 +0500

    правки по странице услуг

--- components/services/Service.vue
+++ components/services/Service.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="py-[24px] px-[14px] border-b border-[var(--color-textgray)] flex justify-between items-center transition-bg-color duration-600"
+    class="py-[24px] px-[14px] border-b border-[var(--color-textgray)] flex justify-between items-center transition-bg-color duration-600 gap-[10px]"
   >
     <p class="text-[var(--color-main)] font-bold">{{ name }}</p>
-    <p class="text-[var(--color-main)] text-[18px] font-bold">{{ value }} Грн</p>
+    <p class="text-[var(--color-main)] text-[18px] font-bold text-nowrap">{{ value }} Грн</p>
   </div>
 </template>
 
