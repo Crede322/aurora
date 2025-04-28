c6f0cf419b1c9a6dc468517918f423d5709b98f8
Crede322 <burhanov774@gmail.com>
2025-04-28 13:11:29 +0500

    правки по футеру

--- components/shared/Contacts.vue
+++ components/shared/Contacts.vue
@@ -6,12 +6,16 @@
     <h3 class="font-bold text-[var(--color-main)]">Наші контакти</h3>
     <div class="inner-row flex gap-[70px] pt-[20px]">
       <div class="numbers flex flex-col gap-[6px]">
-        <a class="leading-none text-[18px]" href="tel:+380977665611">+ 38 (097) 766-5611</a>
-        <a class="leading-none text-[18px]" href="tel:+380507735620">+ 38 (050) 773-5620</a>
+        <a class="leading-none text-[18px] hover:text-[var(--color-main)]" href="tel:+380665171324"
+          >+ 38 (066) 517-1324</a
+        >
+        <a class="leading-none text-[18px] hover:text-[var(--color-main)]" href="tel:+380665171324"
+          >+ 38 (066) 517-1324</a
+        >
 
         <a class="link_geo relative flex gap-1.5 items-center" href="#"
           ><img class="w-5 h-5" src="/img/mark.svg" alt="geo" />
-          <h3>вулиця Остафія Дашкевича 26, Київ</h3>
+          <h3 class="text-[var(--color-main)]">вулиця Остафія Дашкевича 26, Київ</h3>
           <div class="link_underline absolute w-[100%] h-[0.5px] bg-[#145265] bottom-[-2px]"></div
         ></a>
       </div>
@@ -23,5 +27,13 @@
 <style scoped>
 .wrapper__contacts a {
   color: var(--color-main);
+}
+
+.link_underline {
+  opacity: 0;
+  transition: opacity, 0.3s;
+}
+.link_geo:hover .link_underline {
+  opacity: 1;
 }
 </style>
